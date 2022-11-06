7dad9668a0d0291c46070f3cc2ba92a6ba709f3d
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-06 17:40:49 +0200

    Modificated TAsk # 2 -2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,30 @@
 import throttle from 'lodash.throttle';
 import vimioPlayer from '@vimeo/player';
 
+// Connect to HTML and VIMEO
 const iframe = document.querySelector('iframe');
 const player = new vimioPlayer(iframe);
 
+// Video player`s listener
 player.on(
   'timeupdate',
   throttle(onPlayVideo, 1001, {
-    leading: true,
-    trailing: false,
+    leading: false,
+    trailing: true,
   })
 );
 
+// Other func.
 function onPlayVideo(evt) {
-  localStorage.setItem('videoplayer-current-time', evt.seconds);  
-}
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+  }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(hasTimeInStorage());
+
+function hasTimeInStorage() {
+  if (localStorage.getItem('videoplayer-current-time') === null) {
+    return 0;
+  } else {
+    return localStorage.getItem('videoplayer-current-time');
+  }
+}
