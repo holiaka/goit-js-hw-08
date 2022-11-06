3380b9e2629ca9bfa452996246a014a1464b94b9
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-06 15:41:48 +0200

    Added task #2 in JS HW #8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const imageBoxRef = document.querySelector(".gallery");
+imageBoxRef.style.listStyleType = "none";
+
+const imageElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}" /></a></li>`
+);
+
+imageBoxRef.insertAdjacentHTML("beforeend", imageElements.join(""));
+
+// Added the functional from SimpleLightbox's library
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+const bodyRef = document.querySelector("body");
+
+gallery.on("shown.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();  
+});
+
+gallery.on("changed.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+});
+
+gallery.on("close.simplelightbox", function () {
+  bodyRef.style.backgroundColor = "#ffffff";
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+// Load libraries
+import throttle from 'lodash.throttle';
+import vimioPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new vimioPlayer(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(onPlayVideo, 1001, {
+    leading: true,
+    trailing: false,
+  })
+);
+
+function onPlayVideo(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);  
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
